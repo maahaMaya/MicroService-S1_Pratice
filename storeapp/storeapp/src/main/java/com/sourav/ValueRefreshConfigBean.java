0df142fec975587a6198383d46f4cfaf650dc8a9
sourav <devbhsourav@gmail.com>
2023-08-27 16:54:25 +0530

    [sourav]add: springCloudBus

---
+++ storeapp/storeapp/src/main/java/com/sourav/ValueRefreshConfigBean.java
@@ -0,0 +1,25 @@
+package com.sourav;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.stereotype.Component;
+
+@Component
+@RefreshScope
+public class ValueRefreshConfigBean {
+
+	private String color;
+
+	public ValueRefreshConfigBean(@Value("${application.theme.color}") String color) {
+		this.color = color;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+}
