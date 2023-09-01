068edadc6f53227f671fe0cfe609072700c678cc
sourav <devbhsourav@gmail.com>
2023-09-01 14:46:25 +0530

    [sourav]add: apiGateWay-EurekaFeign-Resilience4J-Without-FallbackClass

--- storeApp-Consumer-Eureka-Feign-Resilience4J/storeApp-Consumer-Eureka-Feign-Resilience4J/src/main/java/com/sourav/proxy/ProductServiceProxy.java
+++ storeApp-Consumer-Eureka-Feign-Resilience4J/storeApp-Consumer-Eureka-Feign-Resilience4J/src/main/java/com/sourav/proxy/ProductServiceProxy.java
@@ -9,23 +9,26 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import com.sourav.domain.Product;
-import com.sourav.fallback.ProductServiceFallback;
 
-@FeignClient(name = "product-service", fallback = ProductServiceFallback.class)
-//@FeignClient(name = "product-service")
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+
+@FeignClient(name = "product-service")
 public interface ProductServiceProxy {
 	
+	@CircuitBreaker(name = "product-service", fallbackMethod = "fallbackMethodGetAllProduct")
 	@GetMapping(value = "/product", produces = {MediaType.APPLICATION_JSON_VALUE})
 	public List<Product> getAllProduct();
 	
+	@CircuitBreaker(name = "product-service", fallbackMethod = "fallbackMethodGetProductById")
 	@GetMapping(value = "/product/{id}", produces = {MediaType.APPLICATION_JSON_VALUE})
 	public Product getProductById(@PathVariable("id") Integer id);
 	
-//	public default List<Product> fallbackMethodGetAllProduct() {
-//		return new ArrayList<Product>();
-//	}
-//
-//	public default Product fallbackMethodGetProductById(Integer id) {
-//		return new Product(id, "Monitor", "Jio", 2669.00);
-//	}
+	public default List<Product> fallbackMethodGetAllProduct() {
+		return new ArrayList<Product>();
+	}
+
+	public default Product fallbackMethodGetProductById(Integer id, Throwable cause) {
+		System.out.println("Exception raise with message:===> "+ cause.getMessage());
+		return new Product(id, "Monitor", "Jio", 2669.00);
+	}
 }
