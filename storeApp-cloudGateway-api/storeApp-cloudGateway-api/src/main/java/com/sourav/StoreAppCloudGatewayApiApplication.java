4131fee83b28a2a026144fc293041f13b51fe111
sourav <devbhsourav@gmail.com>
2023-09-04 17:51:32 +0530

    [sourav]add: CloudGateWayApi-Routes

---
+++ storeApp-cloudGateway-api/storeApp-cloudGateway-api/src/main/java/com/sourav/StoreAppCloudGatewayApiApplication.java
@@ -0,0 +1,15 @@
+package com.sourav;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class StoreAppCloudGatewayApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreAppCloudGatewayApiApplication.class, args);
+	}
+
+}
